25c6ef9aafd9db660332e5e37f50408148b5ef27
Fernando Fong <Light_Argent@localhost.localdomain>
2020-05-01 22:24:32 -0500

    Updating

--- SweepLine/State.java
+++ SweepLine/State.java
@@ -1,16 +1,9 @@
 public class State {
 
-    public Point root;
+    public Segment root;
     public State parent;
     public State left;
     public State right;
     public int height;
 
-    public void add(Point p) {
-        State s = new State();
-        s.root = p;
-        if(this.root.compareTo(p) <= 0) {
-            if(this.right == null)
-        }
-    }
 }
