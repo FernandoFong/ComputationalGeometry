cd929ad9fa867d9f5683a1c3f12a258001cd97b1
Fernando Fong <fernandofong@ciencias.unam.mx>
2021-01-12 18:37:15 -0600

    Updating branch

--- SweepLine/C++/SweepLine.cpp
+++ SweepLine/C++/SweepLine.cpp
@@ -15,26 +15,31 @@
 using namespace std;
 
 bool comp_points(Point p1, Point p2) {
-  
+  return false;
 }
 
 void print_seg(Segment s) {
   int x1 = get_x1(s), y1 = get_y1(s), x2 = get_x2(s), y2 = get_y2(s);
-  printf("(%d, %d), (%d, %d)\n", x1, y1, x2, y2);
+  printf("Segment[(%d, %d), (%d, %d)]\n", x1, y1, x2, y2);
 }
 
 int main() {
-  int n;
-  cin >> n;
+  int size;
+  cout << "Type the number of segments:" << endl;
+  cin >> size;
+  cout << "Type the coordinates of the segments" << endl;
   vector<Segment> segs;
-  while(n--) {
+  int n = 0;
+  while(n < size) {
     int x1, y1, x2, y2;
+    printf("Type the %dth segment x1 y1 x2 y2\n", n+1);
     cin >> x1;
     cin >> y1;
     cin >> x2;
     cin >> y2;
     Segment s = nS(x1, y1, x2, y2);
     segs.push_back(s);
+    n++;
   }
   for(Segment s : segs) {
     print_seg(s);
